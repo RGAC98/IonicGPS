8d37bab29b499a5b766aca0ff2f3e92f79b33d0a
RGAC98 <59809286+RGAC98@users.noreply.github.com>
2021-07-26 18:49:37 -0500

    feat: geolocalizador
    Se ha codificado los métodos para obtener las coordenadas latitud y longitud

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {Geolocation, Geoposition} from '@ionic-native/geolocation/ngx'
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  latitud: number
+  longitud: number
+  codigo_gps: any
 
+  constructor(public geolocation: Geolocation) 
+  {}
+
+  getGeolocation()
+  {
+    document.getElementById("codigo-gps").innerHTML = this.codigo_gps;
+    console.log(this.codigo_gps);
+    this.geolocation.getCurrentPosition().then((geoposition: Geoposition) => {
+      this.latitud = geoposition.coords.latitude;
+      this.longitud = geoposition.coords.longitude;
+    }).catch((error) => {
+      console.log("Ha ocuurido un error mientras se obtenian las coordenadas geograficas");
+      console.log(error);
+    });
+  }
 }
