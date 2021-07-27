45f60bf1db018668f4e1fe6ff22624eab3db66fd
RGAC98 <59809286+RGAC98@users.noreply.github.com>
2021-07-27 00:46:47 -0500

    feat: envio de coordenadas al servidor
    Se ha codificado un método que envía coordenadas a la base de datos desplegada en heroku a través del consumo de una api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import {Geolocation, Geoposition} from '@ionic-native/geolocation/ngx'
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -11,21 +12,48 @@ export class HomePage {
 
   latitud: number
   longitud: number
-  codigo_gps: any
+  codigo_gps: string
 
-  constructor(public geolocation: Geolocation) 
+  constructor(public geolocation: Geolocation, public http: HttpClient)
   {}
+
+  postEnviarCoordenadas(codigo_gps, latitud, longitud)
+  {
+    return this.http.post(`https://geo-sam-backend.herokuapp.com/ubicacion/${codigo_gps}/${latitud}/${longitud}`, {});
+  }
 
   getGeolocation()
   {
     document.getElementById("codigo-gps").innerHTML = this.codigo_gps;
-    console.log(this.codigo_gps);
     this.geolocation.getCurrentPosition().then((geoposition: Geoposition) => {
       this.latitud = geoposition.coords.latitude;
       this.longitud = geoposition.coords.longitude;
     }).catch((error) => {
-      console.log("Ha ocuurido un error mientras se obtenian las coordenadas geograficas");
-      console.log(error);
-    });
+      document.getElementById("error-ocurrido").innerHTML = "Ha ocuurido un error"
+    });    
+  }
+
+  enviarCoordenadasTemporizado()
+  {
+    if(this.codigo_gps != undefined)
+    {
+      for (let i = 0; i < 5; i++) 
+      {
+       setTimeout(() => {this.getGeolocation()}, 5000);
+
+        try 
+        {
+          this.postEnviarCoordenadas(this.codigo_gps, this.latitud, this.longitud).subscribe((respuesta: any) => {
+          document.getElementById("mensaje-envio").innerHTML = "Enviando coordenadas";
+          })
+        } catch (error) 
+        {
+          document.getElementById("error-ocurrido").innerHTML = "Error al enviar las coordenadas";  
+        }
+      }
+    }else
+    {
+      document.getElementById("error-ocurrido").innerHTML = "Ingrese codigo del GPS";
+    }
   }
 }
